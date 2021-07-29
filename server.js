42bd4592f7d17bbb01fbbff5460a4fb58be7d4f2
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-28 21:28:36 -0600

    add uuid package and route for adding a new note

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { fstat } = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid')
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -26,6 +26,26 @@ app.get('/api/notes', (req, res) => {
             res.status(500).json(err);
         } else {
             res.json(JSON.parse(data));
+        }
+    });
+});
+
+// POST for new note
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+        if(err) {
+            res.status(500).json(err);
+        } else {
+            const prvNotes = JSON.parse(data);
+            req.body.id = uuidv4();
+            const newNotes = [req.body, ...prvNotes];
+            fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(newNotes), (err) => {
+                if(err) {
+                    res.status(500).json(err);
+                } else {
+                    res.json(newNotes);
+                }
+            })
         }
     });
 });
