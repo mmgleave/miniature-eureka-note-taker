074fb6dd83ce32bb51177d71ee4d53ca362a6b27
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-28 21:13:23 -0600

    add path and home/notes pages routes

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// notes page
+app.get('/notes', (req, res) =>  {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
 app.listen(PORT, () => {
-    console.log('App is running')
-})
+    console.log('Server Running');
+});
