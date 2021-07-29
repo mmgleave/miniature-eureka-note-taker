11bd1aa42c7a80aaa485419cf1be32b0ce6a852a
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-28 21:19:58 -0600

    api/notes route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { fstat } = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
 const path = require('path');
 
 app.use(express.static('public'));
@@ -15,6 +17,17 @@ app.get('/', (req, res) => {
 // notes page
 app.get('/notes', (req, res) =>  {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// GET for api/notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+        if(err) {
+            res.status(500).json(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
 app.listen(PORT, () => {
